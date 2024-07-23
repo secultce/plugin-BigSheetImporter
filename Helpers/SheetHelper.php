312e00c1253611356a49c828c68411e956391530
Ronny John <me@ronnyjohnti.dev>
2024-07-23 10:50:52 -0300

    feat: Finalizando Helper de validação

--- Helpers/SheetHelper.php
+++ Helpers/SheetHelper.php
@@ -2,8 +2,6 @@
 
 namespace BigSheetImporter\Helpers;
 
-use BigSheetImporter\Entities\ImportOccurrence;
-use BigSheetImporter\Entities\Sheet;
 use MapasCulturais\App;
 use MapasCulturais\Entities\Registration;
 use Shuchkin\SimpleXLS;
@@ -13,65 +11,46 @@ final class SheetHelper
 {
     /**
      * @param SimpleXLSX|SimpleXLS $sheet
-     * @return bool
+     * @return array
      */
-    public static function validate($sheet): bool
+    public static function validate($sheet): array
     {
-        $valid = true;
+        $invalidData = [];
 
 
         foreach ($sheet->rows() as $key => $row) {
-            if($key === 0) continue;
-            self::validateRow($row, $valid);
+            if($key === 0)
+                continue;
+
+            $invalidData = self::validateRow($row, $key, $invalidData);
         }
 
-        return $valid;
+        return $invalidData;
     }
 
-    public static function validateRow(array $row, &$valid): void
+    public static function validateRow(array $row, int $rowIndex, array $invalidData = []): array
     {
         $row = array_map(function ($cell) {
             return $cell === '' ? null : $cell;
         }, $row);
 
         $app = App::getInstance();
-        $invalidRegistration = empty($app->repo(Registration::class)->findBy(['number' => $row[0]]));
-        $invalidProcess = is_null($row[1])
-            ? $row[1]
-            : !!preg_match('/[0-9]{5}\.[0-9]{6}\/[0-9]{4}-[0-9]{2}/m', $row[1]);
-        $invalidDate = [];
+        if (empty($app->repo(Registration::class)->findBy(['number' => $row[0]]))) {
+            $invalidData[] = [$rowIndex, 0, 'Inscrição inválida', $row[0]];
+        }
+        if ($row[1] !== null && !!preg_match('/[0-9]{5}\.[0-9]{6}\/[0-9]{4}-[0-9]{2}/m', $row[1])) {
+            $invalidData[] = [$rowIndex, 1, 'Processo inválido', $row[1]];
+        }
         for ($k=6;$k<15;$k++) {
-            if($row[$k] !== null)
-                $invalidDate[] = self::validateDateString($row[$k]);
+            if($row[$k] !== null && !self::validateDateString($row[$k]))
+                $invalidData[] = [$rowIndex, $k, 'Formato de data inválida', $row[$k]];
         }
 
-        foreach (array_keys($invalidDate, true) as $index) {
-            $index +=6;
-
-        }
-        /** @todo: Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate,
-         *         Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate,
-         *         Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate, Validate
-         */
-
+        return $invalidData;
     }
 
     public static function validateDateString(string $dateString): bool
     {
         return !!preg_match('/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/m', $dateString);
-    }
-
-    /**
-     * @param bool[] $occurrences
-     * @param Sheet $sheet
-     * @return array
-     */
-    public static function generateOccurrences(array $occurrences, Sheet $sheet): array
-    {
-        $occurrences = array_map(function ($occurrence, $key) use ($sheet) {
-            return new ImportOccurrence($occurrence, $sheet, $key + 1);
-        }, $occurrences, array_keys($occurrences));
-
-        return $occurrences;
     }
 }
