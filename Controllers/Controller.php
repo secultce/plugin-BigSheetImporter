14b12868898b80958339b640c2e190f479e72aed
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2024-08-20 08:56:27 -0300

    feat: verificação de token de acesso (endpoint atualização status da notificação)

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -199,6 +199,10 @@ class Controller extends \MapasCulturais\Controller
 
     public function POST_updateNotificationStatus()
     {
+        if (!isset($_SERVER['HTTP_ACCESS_TOKEN']) || $_SERVER['HTTP_ACCESS_TOKEN'] !== $_ENV['ACCESS_TOKEN_API_EMAIL']) {
+            $this->json(['message' => 'Acesso não autorizado'], 401);
+        }
+
         $rowSheet = App::i()->repo(RowSheet::class)->findOneBy(['registrationNumber' => $this->data["registration_number"]]);
 
         $rowSheet->updateNotificationStatus();
